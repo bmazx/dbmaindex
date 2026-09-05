70a3211b3c6ac004dabc9369a08e0f608cfdc21f
Brian <brianma360@gmail.com>
2026-09-05 15:51:03 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# dbmaindex
+
+My personal website to host my stuff.
+
+This website does not require any external dependencies to run.
+
+It is recommended to view this website through a server rather than locally on your filesystem due to resolving root directory paths.
+If you have python installed, simply run this in the website root dir:
+```py
+python -m http.server
+```
+and view locally at `http://localhost:8000/index.html`.
--- scripts.js
+++ scripts.js
@@ -366,13 +366,13 @@ document.querySelectorAll(".side-menu a, .side-menu button, .side-menu summary, 
         click2.currentTime = 0;
         click2.play();
 
-        // if (elem instanceof HTMLAnchorElement && elem.href) {
-        //     e.preventDefault();
-        //
-        //     setTimeout(() => {
-        //         window.location.href = elem.href;
-        //     }, 100);
-        // }
+        if (elem instanceof HTMLAnchorElement && elem.href) {
+            e.preventDefault();
+
+            setTimeout(() => {
+                window.location.href = elem.href;
+            }, 100);
+        }
     });
 });
 
